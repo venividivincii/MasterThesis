a8a6ff930cd5d6202c46fc4a6177976f866bdbaf
Vincent O. Zimmer <vzimmer@mail.uni-mannheim.de>
2024-07-03 08:31:24 +0200

    Working Tokenization for Activity Column. Resources still bugged

--- MultiTaskTransformer/package/processtransformer/data/processor.py
+++ MultiTaskTransformer/package/processtransformer/data/processor.py
@@ -6,6 +6,7 @@ import numpy as np
 from multiprocessing import Pool
 from typing import List, Optional
 import pm4py
+import tensorflow as tf
 from ..constants import Task
 
 class LogsDataProcessor:
@@ -68,29 +69,19 @@ class LogsDataProcessor:
             for additional_column in self._additional_columns:
                 if additional_column == org_column:
                     self._additional_columns[additional_column] = df.columns[idx]
-                    
-        print(f"Additional Columns: {self._additional_columns}")
-        print(f"org_columns: {self._org_columns}")
-        print(f"df columns: {df.columns}")
-        print(f"target_column: {self._target_column}")
-        
         if sort_temporally:
             df.sort_values(by=["time:timestamp"], inplace=True)
-        print("Parsing successful.")
-        print(df.info())
-        print(df.head())
         return df
     
     
     # code columns into dicts of unique classes
-    def _extract_logs_metadata(self, df: pd.DataFrame) -> None:
+    def _extract_logs_metadata(self, df: pd.DataFrame) -> dict:
         """Extracts and saves metadata from the dataframe.
 
         Args:
             df (pd.DataFrame): Input dataframe.
         """
         special_tokens = ["[PAD]", "[UNK]"]
-        activities = list(df["concept:name"].unique())
         columns = ["concept:name"] + self._additional_columns
         
         print("Coding Log Meta-Data...")
@@ -109,19 +100,19 @@ class LogsDataProcessor:
             # mapping for special tokens and unique classes
             coded_activity = {f"{column}##x_word_dict": dict(zip(keys, val))}
             # mapping only for unique classes
-            code_activity_normal = {f"{column}##y_word_dict": dict(zip(activities, range(len(activities))))}
-            # append y_word_dict to x_word_dict
-            coded_activity.update(code_activity_normal)
+            coded_activity.update({f"{column}##y_word_dict": dict(zip(classes, range(len(classes))))})
             # create new dict for coded column
             coded_column = {column: coded_activity}
             # update coded columns dict with coded_column
             coded_columns.update(coded_column)
-            
+        
         # convert into json string
         coded_json = json.dumps(coded_columns)
         
         with open(os.path.join(self._dir_path, f"{self._preprocessing_id}_metadata.json"), "w") as metadata_file:
             metadata_file.write(coded_json)
+            
+        return coded_columns
     
     
     def _compute_num_classes(self, df: pd.DataFrame) -> List[int]:
@@ -135,6 +126,8 @@ class LogsDataProcessor:
         """
         return [df[col].nunique() for col in self._additional_columns]
 
+
+    # TODO: old version --> DELETE
     # def _next_categorical_helper_func(self, df: pd.DataFrame) -> pd.DataFrame:
     #     """Helper function to process next categorical data.
 
@@ -163,8 +156,8 @@ class LogsDataProcessor:
     #     return processed_df
     
 
-    # TODO: 
-    def _next_categorical_helper_func(self, df: pd.DataFrame) -> pd.DataFrame:
+    # processes the column prefixes
+    def _process_column_prefixes(self, df: pd.DataFrame) -> pd.DataFrame:
         """Helper function to process next categorical data for all additional columns.
 
         Args:
@@ -185,7 +178,6 @@ class LogsDataProcessor:
             processed_columns.extend([col, f"{col}_prefix", f"{col}_k", f"{col}_next"])
         
         processed_data = []
-        
         unique_cases = df[case_id].unique()
         
         for case in unique_cases:
@@ -202,26 +194,60 @@ class LogsDataProcessor:
                 processed_data.append(row)
         
         processed_df = pd.DataFrame(processed_data, columns=processed_columns)
-        
         return processed_df
 
 
+    def _tokenize_and_pad_feature(self, prefixes: pd.DataFrame, feature_values: pd.Series, word_dict: dict):
+        # Ensure prefixes is a DataFrame with at least one column
+        if isinstance(prefixes, pd.Series):
+            prefixes = prefixes.to_frame()
 
-    def _process_next_categorical(self, df: pd.DataFrame, train_list: List[str], test_list: List[str]) -> None:
-        """Processes data for the next categorical task.
+        if prefixes.shape[1] == 0:
+            raise ValueError("The 'prefixes' DataFrame must have at least one column.")
 
-        Args:
-            df (pd.DataFrame): Input dataframe.
-            train_list (List[str]): List of training case IDs.
-            test_list (List[str]): List of testing case IDs.
-        """
+        # Determine the maximum length of the sequences in 'prefixes'
+        max_length_prefix = max(len(str(seq).split()) for seq in prefixes.iloc[:, 0])
+        
+        tokenized_prefix = []
+        for seq in prefixes.iloc[:, 0]:
+            tokenized_seq = [word_dict.get(word, 0) for word in str(seq).split()]
+            tokenized_prefix.append(tokenized_seq)
+        
+        tokenized_values = feature_values.apply(lambda x: word_dict.get(x, 0))
+        
+        padded_prefix = tf.keras.preprocessing.sequence.pad_sequences(tokenized_prefix, maxlen=max_length_prefix)
+        padded_prefix_str = [" ".join(map(str, seq)) for seq in padded_prefix]
+        
+        return tokenized_values, padded_prefix_str
+
+
+    def _process_next_categorical(self, df: pd.DataFrame, train_list: List[str], test_list: List[str], metadata: dict) -> None:
         df_split = np.array_split(df, self._pool)
         
         with Pool(processes=self._pool) as pool:
-            processed_df = pd.concat(pool.imap_unordered(self._next_categorical_helper_func, df_split))
+            processed_df = pd.concat(pool.imap_unordered(self._process_column_prefixes, df_split))
         
-        train_df = processed_df[processed_df["case_id"].isin(train_list)]
-        test_df = processed_df[processed_df["case_id"].isin(test_list)]
+        train_df = processed_df[processed_df["case_id"].isin(train_list)].copy()
+        test_df = processed_df[processed_df["case_id"].isin(test_list)].copy()
+        
+        for feature in metadata:
+            if feature == self._target_column:
+                word_dict = metadata[feature][f"{feature}##y_word_dict"]
+            else:
+                word_dict = metadata[feature][f"{feature}##x_word_dict"]
+            prefix_col = f"{feature}_prefix"
+            train_prefix_df = train_df[prefix_col]
+            test_prefix_df = test_df[prefix_col]
+            
+            train_tokenized_values, train_padded_prefix = self._tokenize_and_pad_feature(train_prefix_df, train_df[feature], word_dict)
+            
+            train_df.loc[:, feature] = train_tokenized_values
+            train_df.loc[:, prefix_col] = train_padded_prefix
+            
+            test_tokenized_values, test_padded_prefix = self._tokenize_and_pad_feature(test_prefix_df, test_df[feature], word_dict)
+            
+            test_df.loc[:, feature] = test_tokenized_values
+            test_df.loc[:, prefix_col] = test_padded_prefix
         
         train_df.to_csv(os.path.join(self._dir_path, f"{self._preprocessing_id}_train.csv"), index=False)
         test_df.to_csv(os.path.join(self._dir_path, f"{self._preprocessing_id}_test.csv"), index=False)
@@ -245,13 +271,13 @@ class LogsDataProcessor:
             print(f"No preprocessed train-test split for task {task_string} found. Preprocessing...")
         
             df = self._load_df(sort_temporally)
-            self._extract_logs_metadata(df)
+            metadata = self._extract_logs_metadata(df)
             train_test_split_point = int(abs(df["case:concept:name"].nunique() * train_test_ratio))
             train_list = df["case:concept:name"].unique()[:train_test_split_point]
             test_list = df["case:concept:name"].unique()[train_test_split_point:]
             
             if task == Task.NEXT_CATEGORICAL:
-                self._process_next_categorical(df, train_list, test_list)
+                self._process_next_categorical(df, train_list, test_list, metadata)
             elif task == Task.NEXT_TIME:
                 self._process_next_time(df, train_list, test_list)
             elif task == Task.REMAINING_TIME:
